0869ad7ed864b9fefb8baeb64ea8aef967b21818
Manuel-Lauriola <manuelauriol@gmail.com>
2025-07-09 17:37:55 +0200

    [UPD] - button works properly

--- assets/script.js
+++ assets/script.js
@@ -1,3 +1,13 @@
 //dichiaro le variabili che andrò a modificare
 const button = document.getElementById(`button`)
-const lamp = document.getElementById(`lamp`)
+const lampOff = document.getElementById(`lampOff`)
+const lampOn = document.getElementById(`lampOn`)
+
+//definisco la funzione del bottone
+button.addEventListener(`click`, function() {
+  if (button.innerHTML.includes(`ON`)) {
+    button.innerHTML = `OFF`
+  }
+    else {button.innerHTML = `ON`}
+}
+)
